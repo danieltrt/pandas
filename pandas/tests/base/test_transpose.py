179ad987023dd15cafeb9849a9fb948b18c6dba0
Daniel Ramos <daniel.r.ramos@icloud.com>
2023-03-15 15:01:52 -0400

    Update test_transpose.py

--- pandas/tests/base/test_transpose.py
+++ pandas/tests/base/test_transpose.py
@@ -17,9 +17,9 @@ def test_transpose_non_default_axes(index_or_series_obj):
     msg = "the 'axes' parameter is not supported"
     obj = index_or_series_obj
     with pytest.raises(ValueError, match=msg):
-        obj.transpose(1)
+        obj.my_transpose(1)
     with pytest.raises(ValueError, match=msg):
-        obj.transpose(axes=1)
+        obj.my_transpose(axes=1)
 
 
 def test_numpy_transpose(index_or_series_obj):
